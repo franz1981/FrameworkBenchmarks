be5eceb6520ebd4d1c25169bd21f484bfb68f101
John OHara <johara@redhat.com>
2021-11-11 20:44:25 +0000

    Modify Fortunes repo to not use Criteria Query API

--- frameworks/Java/quarkus/reactive-routes-pgclient/src/main/java/io/quarkus/benchmark/resource/FortuneResource.java
+++ frameworks/Java/quarkus/reactive-routes-pgclient/src/main/java/io/quarkus/benchmark/resource/FortuneResource.java
@@ -20,17 +20,12 @@ public class FortuneResource extends BaseResource {
     @Inject
     FortuneRepository repository;
 
-    private Comparator<Fortune> fortuneComparator;
-    private final RockerTemplateEngine templeEngine;
+    private static final Comparator<Fortune> fortuneComparator = Comparator.comparing(fortune -> fortune.getMessage());
+    private static final RockerTemplateEngine templeEngine  = RockerTemplateEngine.create();
 
     private static final String FORTUNES_MAP_KEY = "fortunes" ;
     private static final String FORTUNES_TEMPLATE_FILENAME = "Fortunes.rocker.html" ;
     private static final CharSequence CONTENT_TYPE_HEADER_VALUE = HttpHeaders.createOptimized("text/html; charset=UTF-8");
-
-    public FortuneResource() {
-        templeEngine = RockerTemplateEngine.create();
-        fortuneComparator = Comparator.comparing(fortune -> fortune.getMessage());
-    }
 
     @Route(path = "fortunes")
     public void fortunes(RoutingContext rc) {
