d6b928c5d3144724096449c619a378c69eccee57
Sanne Grinovero <sanne@hibernate.org>
2022-05-19 15:00:11 +0100

    Use the Quarkus Qute template engine

--- frameworks/Java/quarkus/resteasy-reactive-hibernate-reactive/src/main/java/io/quarkus/benchmark/resource/FortuneResource.java
+++ frameworks/Java/quarkus/resteasy-reactive-hibernate-reactive/src/main/java/io/quarkus/benchmark/resource/FortuneResource.java
@@ -1,20 +1,16 @@
 package io.quarkus.benchmark.resource;
 
-import com.fizzed.rocker.Rocker;
-import com.fizzed.rocker.RockerOutput;
 import io.quarkus.benchmark.model.Fortune;
 import io.quarkus.benchmark.repository.FortuneRepository;
+import io.quarkus.qute.Template;
 import io.smallrye.context.api.CurrentThreadContext;
 import io.smallrye.mutiny.Uni;
-import io.vertx.core.http.HttpHeaders;
-import io.vertx.ext.web.templ.rocker.RockerTemplateEngine;
 import org.eclipse.microprofile.context.ThreadContext;
 
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import java.util.Collections;
 import java.util.Comparator;
 
 @Path("/fortunes")
@@ -23,11 +19,10 @@ public class FortuneResource  {
     @Inject
     FortuneRepository repository;
 
-    private static final Comparator<Fortune> fortuneComparator = Comparator.comparing(fortune -> fortune.getMessage());
-    private static final RockerTemplateEngine templeEngine  = RockerTemplateEngine.create();
+    @Inject
+    Template fortunes;
 
-    private static final String FORTUNES_MAP_KEY = "fortunes";
-    private static final String FORTUNES_TEMPLATE_FILENAME = "Fortunes.rocker.html";
+    private static final Comparator<Fortune> fortuneComparator = Comparator.comparing(fortune -> fortune.getMessage());
 
     @Produces("text/html; charset=UTF-8")
     @GET
@@ -37,7 +32,7 @@ public class FortuneResource  {
                 .map(fortunes -> {
                     fortunes.add(new Fortune(0, "Additional fortune added at request time."));
                     fortunes.sort(fortuneComparator);
-                    return templeEngine.render(Collections.singletonMap(FORTUNES_MAP_KEY, fortunes), FORTUNES_TEMPLATE_FILENAME).result().toString();
+                    return this.fortunes.data("fortunes", fortunes).render();
                 });
     }
 }
--- frameworks/Java/quarkus/resteasy-reactive-hibernate/src/main/java/io/quarkus/benchmark/resource/FortuneResource.java
+++ frameworks/Java/quarkus/resteasy-reactive-hibernate/src/main/java/io/quarkus/benchmark/resource/FortuneResource.java
@@ -1,9 +1,8 @@
 package io.quarkus.benchmark.resource;
 
-import com.fizzed.rocker.Rocker;
-import com.fizzed.rocker.RockerOutput;
 import io.quarkus.benchmark.model.Fortune;
 import io.quarkus.benchmark.repository.FortuneRepository;
+import io.quarkus.qute.Template;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
@@ -12,8 +11,6 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
@@ -26,22 +23,18 @@ public class FortuneResource {
     @Inject
     FortuneRepository repository;
 
-    private static final String FORTUNES_MAP_KEY = "fortunes";
-    private static final String FORTUNES_TEMPLATE_FILENAME = "Fortunes.rocker.html";
+    @Inject
+    Template fortunes;
 
     private static final Comparator<Fortune> fortuneComparator = Comparator.comparing(fortune -> fortune.getMessage());
 
     @GET
     @Path("/fortunes")
     public String fortunes() {
-        List<Fortune> fortunes = new ArrayList<>(repository.findAllStateless());
+        List<Fortune> fortunes = repository.findAllStateless();
         fortunes.add(new Fortune(0, "Additional fortune added at request time."));
         fortunes.sort(fortuneComparator);
-
-        RockerOutput output = Rocker.template(FORTUNES_TEMPLATE_FILENAME)
-                .bind(Collections.singletonMap(FORTUNES_MAP_KEY, fortunes))
-                .render();
-
-        return output.toString();
+        return this.fortunes.data("fortunes", fortunes).render();
     }
+
 }
