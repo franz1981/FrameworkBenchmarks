58b34fbf8e8696623e1bdfa468034a36e25026ad
Sanne Grinovero <sanne@hibernate.org>
2022-05-20 12:32:53 +0100

    Revert "Use the Quarkus Qute template engine" - we're not ready for it
    This reverts commit d6b928c5d3144724096449c619a378c69eccee57.
    
    (cherry picked from commit f565b3c88fcbb435ab80d8f56c748984b56ebb72)

--- frameworks/Java/quarkus/resteasy-reactive-hibernate/src/main/java/io/quarkus/benchmark/resource/FortuneResource.java
+++ frameworks/Java/quarkus/resteasy-reactive-hibernate/src/main/java/io/quarkus/benchmark/resource/FortuneResource.java
@@ -1,8 +1,9 @@
 package io.quarkus.benchmark.resource;
 
+import com.fizzed.rocker.Rocker;
+import com.fizzed.rocker.RockerOutput;
 import io.quarkus.benchmark.model.Fortune;
 import io.quarkus.benchmark.repository.FortuneRepository;
-import io.quarkus.qute.Template;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
@@ -11,6 +12,8 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
@@ -23,18 +26,22 @@ public class FortuneResource {
     @Inject
     FortuneRepository repository;
 
-    @Inject
-    Template fortunes;
+    private static final String FORTUNES_MAP_KEY = "fortunes";
+    private static final String FORTUNES_TEMPLATE_FILENAME = "Fortunes.rocker.html";
 
     private static final Comparator<Fortune> fortuneComparator = Comparator.comparing(fortune -> fortune.getMessage());
 
     @GET
     @Path("/fortunes")
     public String fortunes() {
-        List<Fortune> fortunes = repository.findAllStateless();
+        List<Fortune> fortunes = new ArrayList<>(repository.findAllStateless());
         fortunes.add(new Fortune(0, "Additional fortune added at request time."));
         fortunes.sort(fortuneComparator);
-        return this.fortunes.data("fortunes", fortunes).render();
-    }
 
+        RockerOutput output = Rocker.template(FORTUNES_TEMPLATE_FILENAME)
+                .bind(Collections.singletonMap(FORTUNES_MAP_KEY, fortunes))
+                .render();
+
+        return output.toString();
+    }
 }
