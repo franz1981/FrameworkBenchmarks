516553f964226a976387c1aa1b50fe6fb11d1473
franz1981 <nigro.fra@gmail.com>
2022-05-27 08:52:10 +0200

    removing heap sizing and always pretouch

--- frameworks/Java/quarkus/run_quarkus.sh
+++ frameworks/Java/quarkus/run_quarkus.sh
@@ -12,9 +12,6 @@
 JAVA_OPTIONS="-server \
   -Dquarkus.http.io-threads=$((`grep --count ^processor /proc/cpuinfo`)) \
   -Dquarkus.vertx.event-loops-pool-size=$((`grep --count ^processor /proc/cpuinfo`)) \
-  -XX:InitialRAMPercentage=50 \
-  -XX:MaxRAMPercentage=50 \
-  -XX:+AlwaysPreTouch \
   -Dquarkus.vertx.prefer-native-transport=true  \
   -XX:-StackTraceInThrowable \
   -Dquarkus.http.accept-backlog=-1 \
