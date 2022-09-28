28dbc05b8190f29e5834603abfda5b618a6dc37f
franz1981 <nigro.fra@gmail.com>
2022-09-28 14:25:50 +0200

    Adding support for Netty nightly snapshots

--- frameworks/Java/netty/src/main/java/hello/HelloWebServer.java
+++ frameworks/Java/netty/src/main/java/hello/HelloWebServer.java
@@ -11,8 +11,6 @@ import io.netty.channel.epoll.Epoll;
 import io.netty.channel.epoll.EpollChannelOption;
 import io.netty.channel.epoll.EpollEventLoopGroup;
 import io.netty.channel.epoll.EpollServerSocketChannel;
-import io.netty.channel.kqueue.KQueue;
-import io.netty.channel.kqueue.KQueueServerSocketChannel;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.incubator.channel.uring.IOUring;
@@ -41,8 +39,6 @@ public class HelloWebServer {
 		} else
 			if (Epoll.isAvailable()) {
 			doRun(new EpollEventLoopGroup(), EpollServerSocketChannel.class, IoMultiplexer.EPOLL);
-		} else if (KQueue.isAvailable()) {
-			doRun(new EpollEventLoopGroup(), KQueueServerSocketChannel.class, IoMultiplexer.KQUEUE);
 		} else {
 			doRun(new NioEventLoopGroup(), NioServerSocketChannel.class, IoMultiplexer.JDK);
 		}
